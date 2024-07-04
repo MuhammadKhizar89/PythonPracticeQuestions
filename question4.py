86786cbc1ad13b1ea41ed3523591a9dbcf9a3bb4
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-07-04 23:07:44 +0500

    Q4

---
+++ question4.py
@@ -0,0 +1,5 @@
+# Write a program that will give you the sum of 3 digits
+user1=int(input("Enter the first number "));
+user2=int(input("Enter the Second number "));
+user3=int(input("Enter the Third number "));
+print(user1+user2+user3)
