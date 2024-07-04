56d3f25c94aa7726f3b2470ff4a5c3b4bd8b8367
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-07-04 23:00:36 +0500

    Q1

--- question1.py
+++ question1.py
@@ -1,1 +1,5 @@
-print("Hello");
+# User will input (3ages).Find the oldest one
+user1=int(input("Enter the first age "));
+user2=int(input("Enter the Second age "));
+user3=int(input("Enter the Third age "));
+print(max(user1,user2,user3));
