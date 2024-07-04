32da3051d14273f798549b56a5dd85f42a38c2af
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-07-05 00:00:41 +0500

    Q7

---
+++ question7.py
@@ -0,0 +1,6 @@
+# Write a program that will tell whether the given year is a leap year or not.
+user1=int(input("Enter the year "));
+if(user1%4==0):
+    print("Leap Year");
+else :
+    print("Not a Leap Year")
