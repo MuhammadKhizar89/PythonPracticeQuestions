03acdb5b087f2294daa36e5fbac76ca70f3e7b04
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-07-04 23:06:43 +0500

    Q3

---
+++ question3.py
@@ -0,0 +1,8 @@
+# User will input (2numbers).Write a program to swap the numbers
+
+user1=int(input("Enter the first number "));
+user2=int(input("Enter the Second number "));
+user3=user1;
+user1=user2;
+user2=user3;
+print("User1 after swapping ",user1,"User2 after swapping ",user2)
